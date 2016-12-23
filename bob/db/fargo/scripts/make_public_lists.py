8265aba1cf36fe83fb3887e175391a520bd911e3
Guillaume HEUSCH <guillaume.heusch@idiap.ch>
2016-12-23 11:27:34 +0100

    [protocols] clean the script to make protocols file lists

--- bob/db/fargo/scripts/make_public_lists.py
+++ bob/db/fargo/scripts/make_public_lists.py
@@ -7,8 +7,8 @@
 
 Usage:
   %(prog)s [--imagesdir=<path>] 
-           [--protocol=<string>] [--mod=<string>] [--scores] 
-           [--verbose ...] [--plot]
+           [--protocol=<string>] [--mod=<string>] 
+           [--verbose ...] 
 
 Options:
   -h, --help                Show this screen.
@@ -16,9 +16,7 @@ Options:
   -i, --imagesdir=<path>    Where the images are saved.
   -p, --protocol=<int>      The protocol (MC, UD, UO).
   -m, --mod=<int>           The used modality (RGB, NIR, depth) [default: RGB].
-      --scores              If you want to generate score files.
   -v, --verbose             Increase the verbosity (may appear multiple times).
-  -P, --plot                Show some stuff
 
 Example:
 
@@ -43,14 +41,37 @@ from docopt import docopt
 
 version = pkg_resources.require('bob.db.fargo')[0].version
 
-#import numpy
-
-#import bob.io.base
-#import bob.io.image
-
 def build_list(image_dir, ids, channel, sessions, recordings, list_filename, enroll=False):
+  """build_list(image_dir, ids, channel, sessions, recordings, list_filename[, enroll=False])
+  
+  This function load timestamps from a text file.
+  
+  Each line contains two numbers: the frame index and the corresponding time in milliseconds.
+  
+  **Parameters**
+
+    ``images_dir`` (path):
+      The path where the subject images are stored.
+    
+    ``ids`` (list):
+      list of subjects id for this filelist. 
+
+    ``channel`` (string):
+      The type of recorded data (RGB, NIR or depth).
+
+    ``sessions`` (list):
+      The list of sessions considered to build this list.
+
+    ``recordings`` (list):
+      The list of recordings considered to build this list.
+
+    ``list_filename`` (path):
+      The list filename.
+  
+    ``enroll`` (boolean):
+      Set to true if the list is an enrollment list.
   """
-  """
+  
   f = open(list_filename, 'w')
 
   for subject in ids:
@@ -71,10 +92,9 @@ def build_list(image_dir, ids, channel, sessions, recordings, list_filename, enr
 def main(user_input=None):
   """
   
-  Main function to generate  protocol file lists
+  Main function to generate protocol file lists
 
   """
-
   # Parse the command-line arguments
   if user_input is not None:
       arguments = user_input
@@ -96,14 +116,22 @@ def main(user_input=None):
   if args['--verbose'] == 1: logging.getLogger("filelist_log").setLevel(logging.INFO)
   elif args['--verbose'] >= 2: logging.getLogger("filelist_log").setLevel(logging.DEBUG)
 
+  # check user input
   if args['--imagesdir'] is None:
-    logger.warning("You should provide a valid path to the images")
-    sys.exit()
-
+    idiap_dir = '/idiap/project/fargo/databases/images_public/'
+    if os.path.isdir(idiap_dir):
+      args['--imagesdir'] = idiap_dir
+    else:
+      logger.warning("You should provide a valid path to the subjects images")
+      sys.exit()
   if not (args['--protocol'] == 'MC' or args['--protocol'] == 'UD' or args['--protocol'] == 'UO'): 
     logger.warning("Please provide a valid protocol name, {0} is not !".format(args['--protocol']))
     sys.exit()
-   
+  if not (args['--mod'] == 'RGB' or args['--mod'] == 'NIR' or args['--mod'] == 'depth'): 
+    logger.warning("Please provide a valid modality, {0} is not !".format(args['--mod']))
+    sys.exit()
+  
+  # get the modality and the channel
   if args['--mod'] == 'RGB':
     modality = 'RGB'
     channel = 'color'
@@ -113,9 +141,6 @@ def main(user_input=None):
   if args['--mod'] == 'depth':
     modality = 'depth'
     channel = 'depth'
-  if not (args['--mod'] == 'RGB' or args['--mod'] == 'NIR' or args['--mod'] == 'depth'): 
-    logger.warning("Please provide a valid modality, {0} is not !".format(args['--mod']))
-    sys.exit()
   
   # build the correct output dir to store file list
   basedir = 'bob/db/fargo/lists'
@@ -123,7 +148,7 @@ def main(user_input=None):
   if not os.path.isdir(lists_dir):
     os.makedirs(lists_dir)
   
-  # world model
+  # === WORLD MODEL LIST ===
   logger.info("Generating WORLD list for protocol {0} in the {1} channel".format(args['--protocol'], modality))
   world_dir = os.path.join(lists_dir, 'norm') 
   list_filename = os.path.join(world_dir, 'train_world.lst')
@@ -135,60 +160,53 @@ def main(user_input=None):
   recordings =  ['0', '1']
   build_list(args['--imagesdir'], subjects_ids, channel, sessions, recordings, list_filename)
 
-  # development set - enrollment
+  # === ENROLLMENT LISTS === 
+  sessions = ['controlled']
+  recordings =  ['0' ]
+  
+  # dev
   logger.info("Generating enrollment list for DEV set (protocol {0} in the {1} channel)".format(args['--protocol'], modality))
   dev_dir = os.path.join(lists_dir, 'dev')
   list_filename = os.path.join(dev_dir, 'for_models.lst')
   if not os.path.isdir(os.path.dirname(list_filename)):
     os.makedirs(os.path.dirname(list_filename))
   subjects_ids = range(26, 51, 1)
-  sessions = ['controlled']
-  recordings =  ['0' ]
-  build_list(args['--imagesdir'], subjects_ids, channel, sessions, recordings, list_filename, True)
+  build_list(args['--imagesdir'], subjects_ids, channel, sessions, recordings, list_filename, enroll=True)
   
-  # development set - scores
-  logger.info("Generating scores list for DEV set (protocol {0} in the {1} channel)".format(args['--protocol'], modality))
-  dev_dir = os.path.join(lists_dir, 'dev')
-  list_filename = os.path.join(dev_dir, 'for_probes.lst')
-  if not os.path.isdir(os.path.dirname(list_filename)):
-    os.makedirs(os.path.dirname(list_filename))
-  subjects_ids = range(26, 51, 1)
-  if args['--protocol'] == 'MC':
-    sessions = ['controlled']
-    recordings =  ['1' ]
-  if args['--protocol'] == 'UD':
-    sessions = ['dark']
-    recordings =  ['0', '1' ]
-  if args['--protocol'] == 'UO':
-    sessions = ['outdoor']
-    recordings =  ['0''1' ]
-  build_list(args['--imagesdir'], subjects_ids, channel, sessions, recordings, list_filename)
-
-  # eval set - enrollment
+  # eval
   logger.info("Generating enrollment list for EVAL set (protocol {0} in the {1} channel)".format(args['--protocol'], modality))
   eval_dir = os.path.join(lists_dir, 'eval')
   list_filename = os.path.join(eval_dir, 'for_models.lst')
   if not os.path.isdir(os.path.dirname(list_filename)):
     os.makedirs(os.path.dirname(list_filename))
   subjects_ids = range(51, 76, 1)
-  sessions = ['controlled']
-  recordings =  ['0' ]
-  build_list(args['--imagesdir'], subjects_ids, channel, sessions, recordings, list_filename, True)
-  
-  # eval set - scores
+  build_list(args['--imagesdir'], subjects_ids, channel, sessions, recordings, list_filename, enroll=True)
+
+  # === SCORES LIST ===
+  if args['--protocol'] == 'MC':
+    sessions = ['controlled']
+    recordings =  ['1' ]
+  if args['--protocol'] == 'UD':
+    sessions = ['dark']
+    recordings = ['0', '1']
+  if args['--protocol'] == 'UO':
+    sessions = ['outdoor']
+    recordings = ['0', '1']
+
+  # dev
+  logger.info("Generating scores list for DEV set (protocol {0} in the {1} channel)".format(args['--protocol'], modality))
+  dev_dir = os.path.join(lists_dir, 'dev')
+  list_filename = os.path.join(dev_dir, 'for_probes.lst')
+  if not os.path.isdir(os.path.dirname(list_filename)):
+    os.makedirs(os.path.dirname(list_filename))
+  subjects_ids = range(26, 51, 1)
+  build_list(args['--imagesdir'], subjects_ids, channel, sessions, recordings, list_filename)
+
+  # eval
   logger.info("Generating scores list for EVAL set (protocol {0} in the {1} channel)".format(args['--protocol'], modality))
   eval_dir = os.path.join(lists_dir, 'eval')
   list_filename = os.path.join(eval_dir, 'for_probes.lst')
   if not os.path.isdir(os.path.dirname(list_filename)):
     os.makedirs(os.path.dirname(list_filename))
   subjects_ids = range(51, 76, 1)
-  if args['--protocol'] == 'MC':
-    sessions = ['controlled']
-    recordings =  ['1' ]
-  if args['--protocol'] == 'UD':
-    sessions = ['dark']
-    recordings =  ['0', '1' ]
-  if args['--protocol'] == 'UO':
-    sessions = ['outdoor']
-    recordings =  ['0''1' ]
-  build_list(args['--imagesdir'], subjects_ids, channel, sessions, recordings, list_filename, False)
+  build_list(args['--imagesdir'], subjects_ids, channel, sessions, recordings, list_filename)
