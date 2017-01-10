57c5bbdfae099ce02de292e5c18096ab2a7e126c
Guillaume HEUSCH <guillaume.heusch@idiap.ch>
2017-01-10 14:51:56 +0100

    [db] added kwargs in the constructor (mandatory to deal with protocols)

--- bob/db/fargo/query.py
+++ bob/db/fargo/query.py
@@ -10,11 +10,11 @@ class Database(bob.bio.base.database.FileListBioDatabase):
   """Wrapper class for the FARGO database for face verification
   """
 
-  def __init__(self, original_directory=None, original_extension=None):
+  def __init__(self, original_directory=None, original_extension=None, **kwargs):
     # call base class constructor
     from pkg_resources import resource_filename
     folder = resource_filename(__name__, 'lists')
     super(Database, self).__init__(folder, 'fargo', bio_file_class=FaceBioFile,
                                    original_directory=original_directory,
-                                   original_extension=original_extension)
+                                   original_extension=original_extension, **kwargs)
 
