ea1eba7430236389e3ec1954aecab6989c90ccaa
Guillaume HEUSCH <guillaume.heusch@idiap.ch>
2017-01-13 15:36:42 +0100

    [config] added annotation directory

--- bob/db/fargo/config.py
+++ bob/db/fargo/config.py
@@ -8,50 +8,59 @@
 from bob.db.fargo import Database
 
 fargo_images_directory = "/idiap/project/fargo/databases/images_public/"
-fargo_annotations_directory = "/idiap/project/fargo/databases/images_public/"
+fargo_annotations_directory = "/idiap/project/fargo/databases/eyes_center_public/"
 
 database_public_MC_RGB = Database(
     original_directory=fargo_images_directory,
     original_extension=".png",
+    annotation_directory=fargo_annotations_directory, 
     protocol="public_MC_RGB"
 )
 database_public_UD_RGB = Database(
     original_directory=fargo_images_directory,
     original_extension=".png",
+    annotation_directory=fargo_annotations_directory, 
     protocol="public_UD_RGB"
 )
 database_public_UO_RGB = Database(
     original_directory=fargo_images_directory,
     original_extension=".png",
+    annotation_directory=fargo_annotations_directory, 
     protocol="public_UO_RGB"
 )
 database_public_MC_NIR = Database(
     original_directory=fargo_images_directory,
     original_extension=".hdf5",
+    annotation_directory=fargo_annotations_directory, 
     protocol="public_MC_NIR"
 )
 database_public_UD_NIR = Database(
     original_directory=fargo_images_directory,
     original_extension=".hdf5",
+    annotation_directory=fargo_annotations_directory, 
     protocol="public_UD_NIR"
 )
 database_public_UO_NIR = Database(
     original_directory=fargo_images_directory,
     original_extension=".hdf5",
+    annotation_directory=fargo_annotations_directory, 
     protocol="public_UO_NIR"
 )
 database_public_MC_depth = Database(
     original_directory=fargo_images_directory,
     original_extension=".hdf5",
+    annotation_directory=fargo_annotations_directory, 
     protocol="public_MC_depth"
 )
 database_public_UD_depth = Database(
     original_directory=fargo_images_directory,
     original_extension=".hdf5",
+    annotation_directory=fargo_annotations_directory, 
     protocol="public_UD_depth"
 )
 database_public_UO_depth = Database(
     original_directory=fargo_images_directory,
+    annotation_directory=fargo_annotations_directory, 
     original_extension=".hdf5",
     protocol="public_UO_depth"
 )
