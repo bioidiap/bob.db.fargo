bdbc441e2e56841eafe3b270f12290b9f99d9138
Guillaume HEUSCH <guillaume.heusch@idiap.ch>
2017-04-05 14:35:08 +0200

    [config] changed hdf5 to png, add the first protocol for pose varying FR

--- bob/db/fargo/config.py
+++ bob/db/fargo/config.py
@@ -30,37 +30,45 @@ database_public_UO_RGB = Database(
 )
 database_public_MC_NIR = Database(
     original_directory=fargo_images_directory,
-    original_extension=".hdf5",
+    original_extension=".png",
     annotation_directory=fargo_annotations_directory, 
     protocol="public_MC_NIR"
 )
 database_public_UD_NIR = Database(
     original_directory=fargo_images_directory,
-    original_extension=".hdf5",
+    original_extension=".png",
     annotation_directory=fargo_annotations_directory, 
     protocol="public_UD_NIR"
 )
 database_public_UO_NIR = Database(
     original_directory=fargo_images_directory,
-    original_extension=".hdf5",
+    original_extension=".png",
     annotation_directory=fargo_annotations_directory, 
     protocol="public_UO_NIR"
 )
 database_public_MC_depth = Database(
     original_directory=fargo_images_directory,
-    original_extension=".hdf5",
+    original_extension=".png",
     annotation_directory=fargo_annotations_directory, 
     protocol="public_MC_depth"
 )
 database_public_UD_depth = Database(
     original_directory=fargo_images_directory,
-    original_extension=".hdf5",
+    original_extension=".png",
     annotation_directory=fargo_annotations_directory, 
     protocol="public_UD_depth"
 )
 database_public_UO_depth = Database(
     original_directory=fargo_images_directory,
     annotation_directory=fargo_annotations_directory, 
-    original_extension=".hdf5",
+    original_extension=".png",
     protocol="public_UO_depth"
 )
+
+database_public_MC_color_almost_frontal = Database(
+    original_directory='/idiap/project/fargo/database/images_pose_clusters',
+    annotation_directory=None, 
+    original_extension=".png",
+    protocol="public_MC_color_almost_frontal"
+)
+
