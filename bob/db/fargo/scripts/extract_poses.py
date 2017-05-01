528b57c9218398560cef9a3210ef46532b215982
Guillaume HEUSCH <guillaume.heusch@idiap.ch>
2017-05-01 16:50:02 +0200

    [extract pose] modified the script to be able to process NIR and depth images as welle

--- bob/db/fargo/scripts/extract_poses.py
+++ bob/db/fargo/scripts/extract_poses.py
@@ -6,7 +6,7 @@
 """Image extractor for the FARGO videos (%(version)s)
 
 Usage:
-  %(prog)s [--dbdir=<path>] [--imagesdir=<path>] 
+  %(prog)s [--dbdir=<path>] [--imagesdir=<path>] [--mod=<string>] 
            [--verbose ...] [--plot]
 
 Options:
@@ -14,6 +14,7 @@ Options:
   -V, --version             Show version.
   -d, --dbdir=<path>        The path to the database on your disk.
   -i, --imagesdir=<path>    Where to store saved images.
+  -m, --mod=<int>           The used modality (RGB, NIR, depth) [default: RGB].
   -v, --verbose             Increase the verbosity (may appear multiple times).
   -P, --plot                Show some stuff
 
@@ -45,7 +46,7 @@ import bob.io.video
 import bob.io.base
 
 
-def check_if_recording_is_ok(images_dir, subject, session, condition, recording):
+def check_if_recording_is_ok(images_dir, subject, session, condition, recording, channel):
   """check_if_recording_is_ok(images_dir, subject, session, condition, recording) -> [True, False]
   This function checks if the processing of an existing recording is complete.
 
@@ -70,6 +71,9 @@ def check_if_recording_is_ok(images_dir, subject, session, condition, recording)
     ``recording`` (string):
       The recording (0 or 1)
 
+    ``channel`` (string):
+      The channel (color or ir or depth)
+
   **Returns**
 
     ``[True, False]`` (bool)
@@ -84,7 +88,7 @@ def check_if_recording_is_ok(images_dir, subject, session, condition, recording)
   folders.append('pitch_top')
   folders.append('pitch_bottom')
   for folder in folders:
-    if not os.path.isdir(os.path.join(base_path, 'color', folder)):
+    if not os.path.isdir(os.path.join(base_path, channel, folder)):
       return False
   return True
 
@@ -128,6 +132,37 @@ def write_stat(f, pose_text, pose_cluster, n_total_sequences):
   mean_images = pose_cluster / float(n_total_sequences)
   f.write("mean # of images at {0} = {1} ({2} / {3})\n".format(pose_text, mean_images, pose_cluster, n_total_sequences))
 
+
+def pre_process_depth(depth_data):
+  """pre_process_depth(depth_data) -> new_depth_data
+  This function preprocess depth data and return an image
+
+  **Parameters**
+
+    ``depth_data`` (numpy array):
+      The data coming from the depth channel 
+
+  **Returns**
+
+    ``new_depth_data`` (numpy array)
+      The preprocessed depth data, ready to be saved as an image
+  """
+  # get background / foreground (i.e. zero-valued pixels are considered as background)
+  background = numpy.where(depth_data <= 0)
+  foreground = numpy.where(depth_data > 0)
+  
+  # trick such that the highest value is the closest to the sensor
+  depth_data = depth_data * (-1)
+  max_significant = numpy.max(depth_data[foreground])
+  min_significant = numpy.min(depth_data[foreground])
+
+  # normalize to 0-255 and set background to zero
+  new_depth_data = 255 * ((depth_data - min_significant) / float(max_significant -  min_significant))
+  new_depth_data[background] = 0
+
+  return new_depth_data
+
+
 def main(user_input=None):
   """
   
@@ -169,6 +204,21 @@ def main(user_input=None):
     sys.exit()
   base_dir = args['--dbdir']
 
+  if not (args['--mod'] == 'RGB' or args['--mod'] == 'NIR' or args['--mod'] == 'depth'): 
+    logger.warning("Please provide a valid modality, {0} is not !".format(args['--mod']))
+    sys.exit()
+  
+  # get the modality and the channel
+  if args['--mod'] == 'RGB':
+    modality = 'RGB'
+    channel = 'color'
+  if args['--mod'] == 'NIR':
+    modality = 'NIR'
+    channel = 'ir'
+  if args['--mod'] == 'depth':
+    modality = 'depth'
+    channel = 'depth'
+  
   # lists for pose cluster
   yaw_small = []
   yaw_left = []
@@ -187,13 +237,13 @@ def main(user_input=None):
   pitch_bottom_counter = 0
 
   # go through the subjects 
-  subjects = []
-  subjects_ids = range(26, 27, 1)
-  for subject in subjects_ids:
-    subjects.append('{:0>3d}'.format(subject))
-  for subject in subjects:
+  #subjects = []
+  #subjects_ids = range(1, 2, 1)
+  #for subject in subjects_ids:
+  #  subjects.append('{:0>3d}'.format(subject))
+  #for subject in subjects:
   
-  #for subject in os.listdir(base_dir):
+  for subject in os.listdir(base_dir):
 
     sessions = ['controlled', 'dark', 'outdoor']
     # small hack to process FdV subjects ...
@@ -211,21 +261,48 @@ def main(user_input=None):
           stream_dir = os.path.join(args['--dbdir'], subject, session, condition, recording, 'streams')
           
           # create directories to save the extracted data
-          if not os.path.isdir(os.path.join(args['--imagesdir'], subject, session, condition, recording, 'color')):
-            os.makedirs(os.path.join(args['--imagesdir'], subject, session, condition, recording, 'color'))
+          if not os.path.isdir(os.path.join(args['--imagesdir'], subject, session, condition, recording, channel)):
+            os.makedirs(os.path.join(args['--imagesdir'], subject, session, condition, recording, channel))
 
           # check if the recording has already been processed, and if so, that everything is ok  
-          if check_if_recording_is_ok(args['--imagesdir'], subject, session, condition, recording):
+          if check_if_recording_is_ok(args['--imagesdir'], subject, session, condition, recording, channel):
             logger.info("recording already processed and ok")
             continue
 
           # load the files with the timestamps
-          color_annotations_timestamps = load_timestamps(os.path.join(annotation_dir, 'color_timestamps.txt'))
-          color_stream_timestamps = load_timestamps(os.path.join(stream_dir, 'color_timestamps.txt'))
+          annotations_timestamps = load_timestamps(os.path.join(annotation_dir, channel + '_timestamps.txt'))
+          stream_timestamps = load_timestamps(os.path.join(stream_dir, channel + '_timestamps.txt'))
 
           # load the video sequence
-          seq = bob.io.video.reader(os.path.join(stream_dir, 'color', 'color.mov'))
-          sequence = seq.load()
+          if channel == 'color':
+            seq = bob.io.video.reader(os.path.join(stream_dir, 'color', 'color.mov'))
+            sequence = seq.load()
+          if channel == 'ir':
+            # uncompress the 7z archive - ir - if needed
+            ir_dir = os.path.join(stream_dir, 'ir')
+            if (len(os.listdir(ir_dir))) == 1 and ('ir.7z' in os.listdir(ir_dir)):
+              logger.debug("uncompressing NIR")
+              ir_compressed = os.path.join(ir_dir, 'ir.7z')
+              command = "7z x -y -o" + ir_dir + ' ' + ir_compressed
+              try:
+                p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                stdoutdata, stderrdata = p.communicate()
+              except:
+                pass
+          if channel == 'depth':
+            # uncompress the 7z archive - depth - if needed
+            depth_dir = os.path.join(stream_dir, 'depth')
+            if (len(os.listdir(depth_dir))) == 1 and ('depth.7z' in os.listdir(depth_dir)):
+              logger.debug("uncompressing depth")
+              depth_compressed = os.path.join(depth_dir, 'depth.7z')
+              command = "7z x -y -o" + depth_dir + ' ' + depth_compressed
+              try:
+                p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                stdoutdata, stderrdata = p.communicate()
+              except:
+                pass
+
+          
           n_sequences += 1
 
           # empty the lists
@@ -243,22 +320,107 @@ def main(user_input=None):
             # the folder where images will be stored depend on the annotation index
 
             # retrieve the past timestamps  - up to the previous annotated image 
-            previous_stamps = retrieve_past_timestamps(color_annotations_timestamps[i], color_stream_timestamps, color_annotations_timestamps[i-1])
+            previous_stamps = retrieve_past_timestamps(annotations_timestamps[i], stream_timestamps, annotations_timestamps[i-1])
             
             # get the frames in the interval
             counter = 0
             for index in sorted(previous_stamps, reverse=True):
 
               # check the eligibility conditions on this frame 
-              if previous_stamps[index] > color_annotations_timestamps[i-1] and counter < 5:
+              if previous_stamps[index] > annotations_timestamps[i-1] and counter < 5:
                 
                 # put this image in the correct list
-                if i == 1 or i == 4: yaw_small.append(sequence[index])
-                if i == 2 or i == 3: yaw_left.append(sequence[index])
-                if i == 5 or i== 6: yaw_right.append(sequence[index])
-                if i == 7 or i == 10: pitch_small.append(sequence[index])
-                if i == 8 or i == 9: pitch_top.append(sequence[index])
-                if i == 11 or i == 12: pitch_bottom.append(sequence[index])
+
+                # yaw small
+                if i == 1 or i == 4: 
+                  if channel == 'color':
+                    yaw_small.append(sequence[index])
+                  if channel == 'ir':
+                    ir_file = os.path.join(ir_dir, '{0}.bin'.format(index))
+                    ir_data = numpy.fromfile(ir_file, dtype=numpy.int16).reshape(-1, 640)
+                    ir_image = ir_data / 4.0 
+                    yaw_small.append(ir_image)
+                  if channel == 'depth':
+                    depth_file = os.path.join(depth_dir, '{0}.bin'.format(index))
+                    depth_data = numpy.fromfile(depth_file, dtype=numpy.int16).reshape(-1, 640)
+                    depth_image = pre_process_depth(depth_data)
+                    yaw_small.append(depth_image)
+                
+                # yaw left
+                if i == 2 or i == 3: 
+                  if channel == 'color':
+                    yaw_left.append(sequence[index])
+                  if channel == 'ir':
+                    ir_file = os.path.join(ir_dir, '{0}.bin'.format(index))
+                    ir_data = numpy.fromfile(ir_file, dtype=numpy.int16).reshape(-1, 640)
+                    ir_image = ir_data / 4.0 
+                    yaw_left.append(ir_image)
+                  if channel == 'depth':
+                    depth_file = os.path.join(depth_dir, '{0}.bin'.format(index))
+                    depth_data = numpy.fromfile(depth_file, dtype=numpy.int16).reshape(-1, 640)
+                    depth_image = pre_process_depth(depth_data)
+                    yaw_left.append(depth_image)
+                
+                # yaw right
+                if i == 5 or i== 6: 
+                  if channel == 'color':
+                    yaw_right.append(sequence[index])
+                  if channel == 'ir':
+                    ir_file = os.path.join(ir_dir, '{0}.bin'.format(index))
+                    ir_data = numpy.fromfile(ir_file, dtype=numpy.int16).reshape(-1, 640)
+                    ir_image = ir_data / 4.0 
+                    yaw_right.append(ir_image)
+                  if channel == 'depth':
+                    depth_file = os.path.join(depth_dir, '{0}.bin'.format(index))
+                    depth_data = numpy.fromfile(depth_file, dtype=numpy.int16).reshape(-1, 640)
+                    depth_image = pre_process_depth(depth_data)
+                    yaw_right.append(depth_image)
+                
+                # pitch small
+                if i == 7 or i == 10: 
+                  if channel == 'color':
+                    pitch_small.append(sequence[index])
+                  if channel == 'ir':
+                    ir_file = os.path.join(ir_dir, '{0}.bin'.format(index))
+                    ir_data = numpy.fromfile(ir_file, dtype=numpy.int16).reshape(-1, 640)
+                    ir_image = ir_data / 4.0 
+                    pitch_small.append(ir_image)
+                  if channel == 'depth':
+                    depth_file = os.path.join(depth_dir, '{0}.bin'.format(index))
+                    depth_data = numpy.fromfile(depth_file, dtype=numpy.int16).reshape(-1, 640)
+                    depth_image = pre_process_depth(depth_data)
+                    pitch_small.append(depth_image)
+                
+                # pitch top
+                if i == 8 or i == 9: 
+                  if channel == 'color':
+                    pitch_top.append(sequence[index])
+                  if channel == 'ir':
+                    ir_file = os.path.join(ir_dir, '{0}.bin'.format(index))
+                    ir_data = numpy.fromfile(ir_file, dtype=numpy.int16).reshape(-1, 640)
+                    ir_image = ir_data / 4.0 
+                    pitch_top.append(ir_image)
+                  if channel == 'depth':
+                    depth_file = os.path.join(depth_dir, '{0}.bin'.format(index))
+                    depth_data = numpy.fromfile(depth_file, dtype=numpy.int16).reshape(-1, 640)
+                    depth_image = pre_process_depth(depth_data)
+                    pitch_top.append(depth_image)
+                
+                # pitch bottom
+                if i == 11 or i == 12: 
+                  if channel == 'color':
+                    pitch_bottom.append(sequence[index])
+                  if channel == 'ir':
+                    ir_file = os.path.join(ir_dir, '{0}.bin'.format(index))
+                    ir_data = numpy.fromfile(ir_file, dtype=numpy.int16).reshape(-1, 640)
+                    ir_image = ir_data / 4.0 
+                    pitch_bottom.append(ir_image)
+                  if channel == 'depth':
+                    depth_file = os.path.join(depth_dir, '{0}.bin'.format(index))
+                    depth_data = numpy.fromfile(depth_file, dtype=numpy.int16).reshape(-1, 640)
+                    depth_image = pre_process_depth(depth_data)
+                    pitch_bottom.append(depth_image)
+                
               
               counter += 1
 
@@ -266,7 +428,7 @@ def main(user_input=None):
           folders = ['yaw_small', 'yaw_left', 'yaw_right', 'pitch_small', 'pitch_top', 'pitch_bottom']
           for folder in folders:
 
-            to_save_dir = os.path.join(args['--imagesdir'], subject, session, condition, recording, 'color', folder) 
+            to_save_dir = os.path.join(args['--imagesdir'], subject, session, condition, recording, channel, folder) 
             if not os.path.isdir(os.path.join(to_save_dir)):
               os.makedirs(to_save_dir)
             
@@ -281,11 +443,17 @@ def main(user_input=None):
             k = 0
             for image in current_list:
               saved_image = os.path.join(to_save_dir, '{:0>2d}.png'.format(k))
-              bob.io.base.save(image, saved_image)
+              if channel == 'color':
+                bob.io.base.save(image, saved_image)
+              else:
+                bob.io.base.save(image.astype('uint8'), saved_image)
 
               if bool(args['--plot']):
                 from matplotlib import pyplot
-                pyplot.imshow(numpy.rollaxis(numpy.rollaxis(image, 2),2))
+                if channel == 'color':
+                  pyplot.imshow(numpy.rollaxis(numpy.rollaxis(image, 2),2))
+                else:
+                  pyplot.imshow(image, cmap='gray')
                 pyplot.title('image {0} for interval {1}'.format(k, folder))
                 pyplot.show()
  
