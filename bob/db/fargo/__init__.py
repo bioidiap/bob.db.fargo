6a1a29a36f39bb8c38e95734c5795b805ee5999f
Guillaume HEUSCH <guillaume.heusch@idiap.ch>
2018-09-04 13:39:52 +0200

    removed (probable) unecessary imports

--- bob/db/fargo/__init__.py
+++ bob/db/fargo/__init__.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # encoding: utf-8
-# Guillaume HEUSCH <guillaume.heusch@idiap.ch>
-# Thu 22 Dec 16:16:15 CET 2016
 
 
 from .query import Database
 import bob.io.image
 #from bob.bio.base.database.filelist.models import FileListFile, Client
-from bob.bio.base.database.filelist.models import FileListFile
+#from bob.bio.base.database.filelist.models import FileListFile
 
 def get_config():
   """Returns a string containing the configuration information.
