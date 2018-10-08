d5122002ea258ceb171439a80829e9332ff132a5
Guillaume HEUSCH <guillaume.heusch@idiap.ch>
2018-10-08 13:59:47 +0200

    added model_ids method + some fixes

--- bob/db/fargo/query.py
+++ bob/db/fargo/query.py
@@ -26,7 +26,7 @@ class Database(bob.db.base.SQLiteDatabase):
     self.annotation_extension = annotation_extension
     self.protocol = protocol
 
-  def objects(self, groups=None, purpose=None):
+  def objects(self, protocol=None, groups=None, purposes=None, model_ids=None):
     """ Return a set of file
 
     Parameters
@@ -69,7 +69,7 @@ class Database(bob.db.base.SQLiteDatabase):
     list_train = list(q_train)
     #for i in list_train:
     #  print(i)
-    print("number of training images for protocol {} -> {}".format(self.protocol, len(list_train)))
+    #print("number of training images for protocol {} -> {}".format(self.protocol, len(list_train)))
 
     # now get enrollment images for both dev and test
     q_enroll = q.filter(and_(File.client_id > 25, File.purpose == 'enroll'))
@@ -77,7 +77,7 @@ class Database(bob.db.base.SQLiteDatabase):
     #for i in list_enroll:
     #  print(i)
     #print(len(list_enroll))
-    print("number of enrollment images for protocol {} -> {}".format(self.protocol, len(list_enroll)))
+    #print("number of enrollment images for protocol {} -> {}".format(self.protocol, len(list_enroll)))
 
     # now get the probes ...
     q_probe = q.filter(and_(File.client_id > 25, File.purpose == 'probe'))
@@ -92,9 +92,9 @@ class Database(bob.db.base.SQLiteDatabase):
       q_probe = q_probe.filter(and_(File.light == 'outdoor', File.pose == 'frontal'))
       list_probe = list(q_probe)
 
-    for i in list_probe:
-      print(i)
-    print("number of probe images for protocol {} -> {}".format(self.protocol, len(list_probe)))
+    #for i in list_probe:
+    #  print(i)
+    #print("number of probe images for protocol {} -> {}".format(self.protocol, len(list_probe)))
     
     
     final_list = []
@@ -105,25 +105,35 @@ class Database(bob.db.base.SQLiteDatabase):
     if 'dev' in groups:
       q_dev_enroll = q_enroll.filter(File.client_id <= 50)
       q_dev_probe = q_probe.filter(File.client_id <= 50)
-      if purpose is None:
-        list_dev = list(q_dev_enroll) + list(q_dev_probe)
-      if purpose == 'enroll'
+      list_dev = list(q_dev_enroll) + list(q_dev_probe)
+      if purposes is not None and 'enroll' in purposes:
         list_dev = list(q_dev_enroll) 
-      if purpose == 'probe'
-        list_dev =  list(q_dev_probe)
+      if purposes is not None and 'probe' in purposes:
+        list_dev = list(q_dev_probe)
       final_list += list_dev
 
     # evaluation set: ids 51 to 75, enroll and probe
     if 'eval' in groups:
       q_eval_enroll = q_enroll.filter(File.client_id > 50)
       q_eval_probe = q_probe.filter(File.client_id > 50)
-      if purpose is None:
-        list_eval = list(q_eval_enroll) + list(q_eval_probe)
-      if purpose == 'enroll'
+      list_eval = list(q_eval_enroll) + list(q_eval_probe)
+      if purposes is not None and 'enroll' in purposes:
         list_eval = list(q_eval_enroll) 
-      if purpose == 'probe'
+      if purposes is not None and 'probe' in purposes:
         list_eval =  list(q_eval_probe)
       final_list += list_eval
 
     return final_list
-  
+
+
+  def model_ids(self, groups=None, protocol=None, **kwargs): 
+    
+    ids = []
+    if 'train' in groups:
+      ids += list(range(26))
+    if 'dev' in groups:
+      ids += list(range(26, 51))
+    if 'eval' in groups:
+      ids += list(range(51, 76))
+
+    return ids
